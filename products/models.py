a4818023db84f0cd89e680eb48b21b5c30fa60c3
adil-km <adilkmparappur@gmail.com>
2025-04-22 22:51:25 +0530

    db added for customer, orders and themes

--- products/models.py
+++ products/models.py
@@ -10,7 +10,7 @@ class Product(models.Model):
     title = models.CharField(max_length=200)
     price = models.FloatField()
     description = models.TextField()
-    image = models.ImageField(upload_to='/media')
+    image = models.ImageField(upload_to='media/')
     priority = models.IntegerField(default=0)
     delete_status = models.IntegerField(choices=DELETE_CHOICE,default=LIVE)
     created_at = models.DateTimeField(auto_now=True)
